04db43f83c21e05af9a7df91626fdbdd78c273a3
Joachim Goennheimer <Joachim.goennheimer@ibm.com>
2020-05-27 08:35:57 +0200

    Initial Commit

---
+++ Photorama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Joachim Goennheimer on 27.05.20.
+//  Copyright © 2020 Joachim Goennheimer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
