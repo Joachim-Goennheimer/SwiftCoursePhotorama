59bdf53e3bed2d0d96ef620042d1c049da729b43
Joachim Goennheimer <Joachim.goennheimer@ibm.com>
2020-05-27 11:14:42 +0200

    add imageView and spinner to collectionView cells

---
+++ Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by Joachim Goennheimer on 27.05.20.
+//  Copyright © 2020 Joachim Goennheimer. All rights reserved.
+//
+
+// import Foundation
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    func update(with image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        update(with: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        update(with: nil)
+    }
+}
