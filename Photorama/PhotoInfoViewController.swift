e2f27e9300d1a80ef862a0e005d8377ef8f80572
Joachim Goennheimer <Joachim.goennheimer@ibm.com>
2020-05-27 11:52:10 +0200

    implement PhotosInfoViewController

---
+++ Photorama/PhotoInfoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoInfoViewController.swift
+//  Photorama
+//
+//  Created by Joachim Goennheimer on 27.05.20.
+//  Copyright © 2020 Joachim Goennheimer. All rights reserved.
+//
+
+// import Foundation
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    var photo: Photo! {
+        didSet {
+            navigationItem.title = photo.title
+        }
+    }
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchImage(for: photo) {(result) -> Void in
+            switch result {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error fetching image for photo: \(error)")
+            }
+        }
+    }
+    
+}
