29853d84da1a64807ce17ae021f414b0912b9dd8
Joachim Goennheimer <Joachim.goennheimer@ibm.com>
2020-05-27 09:38:05 +0200

    implement JSON serialisation

---
+++ Photorama/Photo.swift
@@ -0,0 +1,24 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by Joachim Goennheimer on 27.05.20.
+//  Copyright © 2020 Joachim Goennheimer. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+    let title: String
+    let remoteURL: URL
+    let photoID: String
+    let dateTaken: Date
+    
+    init(title: String, photoID: String, remoteURL: URL, dateTaken: Date) {
+        self.title = title
+        self.remoteURL = remoteURL
+        self.photoID = photoID
+        self.dateTaken = dateTaken
+    }
+    
+}
