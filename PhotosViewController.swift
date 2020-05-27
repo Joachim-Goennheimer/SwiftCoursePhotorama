72b4fc54299722c50bbe34dd834db246f573fc31
Joachim Goennheimer <Joachim.goennheimer@ibm.com>
2020-05-27 08:46:36 +0200

    implement erstes Photorama Interface

---
+++ PhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Joachim Goennheimer on 27.05.20.
+//  Copyright © 2020 Joachim Goennheimer. All rights reserved.
+//
+
+// import Foundation
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+}
